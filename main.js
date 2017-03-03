dc3ca11d70ed161f876b109356e16f60cb72a60b
Doug Wright <mrdougwright@gmail.com>
2017-03-03 09:41:59 -0800

    modify ext for new api

--- main.js
+++ main.js
@@ -6,8 +6,13 @@ document.addEventListener("DOMContentLoaded", function() {
         var li = document.createElement('li');
         li.innerHTML = `
           <div class='dtc pr4'>${item.asin}</div>
-          <div class='dtc pl3 pr3'>${item.rating}</div>
-          <div class='dtc pl4'>${item.num_ratings}</div>
+          <div class='dtc pr4'>${item.product_name}</div>
+          <div class='dtc pr4'>${item.brand}</div>
+          <div class='dtc pr4'>${item.price}</div>
+          <div class='dtc pr4'>${item.category}</div>
+          <div class='dtc pr4'>${item.rank}</div>
+          <div class='dtc pr4'>${item.rating}</div>
+          <div class='dtc pr4'>${item.review_count}</div>
         `;
         li.className = "pv3 ba b--dotted"
         document.getElementById('zongitems').appendChild(li)
--- scraper.js
+++ scraper.js
@@ -1,37 +1,71 @@
+let data
+let page_scraping = true
 const items = document.getElementById('s-results-list-atf').getElementsByTagName('li')
+let url = "https://localhost:3000/api/v1/items?"
 const asins = Array.from(items).map(getAsinNumber)
+let review_hash = makeScrapedReviewHash(asins)
+const params = asins.map(asin => `asins[]=${asin}&`).join('')
 
 function getAsinNumber(domNode) {
   return domNode.dataset.asin
 }
 
-function getItemData(asins) {
-  let url = "https://localhost:3000/api/v1/items?"
-  const params = asins.map(asin => {
-    return `asins[]=${asin}&`
-  }).join('')
-
-  fetch((url + params), {
-    method: 'GET',
-    mode: 'cors',
-    // headers: new Headers({ 'Content-Type': 'json' }),
-    cache: 'default'
-  }).then(resp => resp.json())
-    .then(data => {
-      // debugger
-      return data
-    }).catch(err => {
-      console.log('error!')
-      console.log(err)
-    })
+function handleErrors(response) {
+  if (!response.ok)
+    throw Error(response.statusText)
+  return response
 }
+
+function makeScrapedReviewHash(asins) {
+  let reviewHash = {}
+  asins.map(asin => {
+    let spans = document.querySelectorAll(`[name=${asin}]`)
+    if (spans.length > 0) {
+      var x = spans[0]
+      var rating = x.getElementsByClassName('a-icon-star')
+      if (rating.length <= 0) {
+        rating = x.getElementsByTagName('i')
+      }
+      reviewHash[asin] = {}
+      reviewHash[asin].rating = rating[0].innerText.match(/[^\s]+/)[0]
+      reviewHash[asin].review_count = x.parentElement.children[1].innerText
+    }
+  })
+  return reviewHash
+}
+
+fetch((url + params), {
+  method: 'GET',
+  mode: 'cors',
+  headers: new Headers({ 'Content-Type': 'json' }),
+  cache: 'default'
+}).then(handleErrors)
+  .then(resp => {
+    console.log('handling json response')
+    return resp.json()
+  })
+  .then(dater => {
+    console.log('got data!')
+    data = dater.map(item => {
+      if (review_hash[item.asin]) {
+        item.rating = review_hash[item.asin].rating
+        item.review_count = review_hash[item.asin].review_count
+      }
+      return item
+    })
+    return
+  })
+  .catch(err => {
+    console.log('error!')
+    console.log(err)
+  })
+
 
 chrome.runtime.onMessage.addListener(
   function(message, sender, sendResponse) {
     switch(message.type) {
       case "getItems":
-        // sendResponse(items_data);
-        getItemData(asins)
+        sendResponse(data)
         break;
       default:
         console.error("Unrecognised message: ", message);
